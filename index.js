348feb0f05a876381342780a21c86211485fc70f
rbrtrss <roberto.rousse@gmail.com>
2021-08-01 02:05:07 -0300

    Limpiando

--- index.js
+++ index.js
@@ -16,47 +16,7 @@ server.on('error', (err) => {
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-// Classes
-// class Productos {
-//     constructor() {
-//         this.productos = [];
-//         this.id = 0;
-//     }
-
-//     showProductos() {
-//         if (this.productos.length == 0) {
-//             return { error: 'No hay productos cargados'}
-//         } else {
-//             return this.productos;
-//         }
-//     }
-
-//     addProducto(title, price, thumbnail) {
-//         this.id++
-//         const producto = {
-//             id: this.id,
-//             title: title,
-//             price: price,
-//             thumbnail: thumbnail,
-//         }
-//         this.productos.push(producto);
-//         return producto;
-//     }
-// 
-    // showUnProducto(id) {
-        // const producto = this.productos[id-1];
-        // if (producto == undefined) {
-            // return { error: 'Producto no encontrado' };
-        // } else {
-            // return producto;
-        // }
-    // }
-// }
-
-// Instances
-// let productos = new Productos();
-
-// Routes
+// Rutas
 app.get('/api/productos/listar', (req, res) => {
     const listaProductos = productos.showProductos();
     res.json(listaProductos);
